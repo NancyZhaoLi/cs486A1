81dcaaac1622c5ce395417a9c5df5918e7eb00d2
NancyZhaoLi <nancy_zhaoying_li@hotmail.com>
2016-09-22 11:51:59 -0400

    init

---
+++ generate.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+
+class Sequence {
+
+}
+
+
+public class generate {
+
+
+
+	public static void main(String[] args) {
+		//
+	}
+
+	String generate(String startingWord, List<String> sentenceSpec, 
+					String searchStrategy, String graph){
+		return "sdf";
+	}
+
+	//Sequence addWordToSequence(Sequence s, String word, String partOf)
+}
